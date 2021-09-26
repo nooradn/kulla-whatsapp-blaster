8d9fea86df5d97b3490d623e500ae9799f238351
Noor Adn <33945704+nooradn@users.noreply.github.com>
2021-09-26 11:06:19 +0700

    revert to old

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:kulla/pages/account.dart';
 import 'package:kulla/panelbar.dart';
+=======
+import 'package:google_fonts/google_fonts.dart';
+>>>>>>> parent of 5937900 (update panel-side)
 import 'package:kulla/resources.dart';
 import 'package:remixicon/remixicon.dart';
 
-void main() => runApp(KullaApp());
+void main() {
+  runApp(KullaApp());
+}
 
 class KullaApp extends StatelessWidget {
   @override
@@ -12,13 +18,21 @@ class KullaApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kulla',
       theme: ThemeData(
+<<<<<<< HEAD
         primarySwatch: Colors.teal,
+=======
+        textTheme: GoogleFonts.ubuntuTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        primarySwatch: Colors.green,
+>>>>>>> parent of 5937900 (update panel-side)
       ),
       home: Row(
         children: [
           /// PANEL SISI KIRI
           Container(
             color: primary,
+<<<<<<< HEAD
             width: 75,
             child: Panel(),
           ),
@@ -30,13 +44,23 @@ class KullaApp extends StatelessWidget {
             padding: EdgeInsets.all(50),
             child: AccountPage(),
           ),
+=======
+            width: 100,
+            // height: 700,
+            child: Panel(),
+          ),
+          Container(
+            color: bg,
+            // width: 1050,
+            // height: 700,
+            // child: ,
+          )
+>>>>>>> parent of 5937900 (update panel-side)
         ],
       ),
     );
   }
 }
-
-// SISI PANEL KIRI
 
 class Panel extends StatelessWidget {
   const Panel({Key? key}) : super(key: key);
@@ -48,6 +72,7 @@ class Panel extends StatelessWidget {
       children: [
         Column(
           children: [
+<<<<<<< HEAD
             PanelBar(
               index: 1,
               icon: Remix.account_circle_line,
@@ -59,16 +84,68 @@ class Panel extends StatelessWidget {
             PanelBar(
               index: 3,
               icon: Remix.contacts_book_2_line,
+=======
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Icon(
+                  Remix.account_circle_line,
+                  size: 41,
+                  color: Colors.white,
+                ),
+              ),
             ),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Icon(
+                Remix.send_plane_line,
+                size: 41,
+                color: Colors.white,
+              ),
+>>>>>>> parent of 5937900 (update panel-side)
+            ),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Icon(
+                Remix.contacts_book_2_line,
+                size: 41,
+                color: Colors.white,
+              ),
+            )
           ],
         ),
-        PanelBar(
-          icon: Remix.broadcast_line,
-          index: 0,
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 25),
+          child: Column(
+            children: [
+              Icon(
+                Remix.broadcast_fill,
+                color: Colors.white,
+                size: 41,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Kulla',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.white,
+                  // fontFamily:
+                ),
+              ),
+            ],
+          ),
         )
       ],
     );
   }
 }
+<<<<<<< HEAD
 
 // KONTEN SISI KANAN
+=======
+>>>>>>> parent of 5937900 (update panel-side)
